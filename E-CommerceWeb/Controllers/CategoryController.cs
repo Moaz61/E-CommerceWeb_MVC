a339f862b49cccda9e7e819908dbc5f482341488
moaz <moaz43700@gmail.com>
2025-06-26 02:28:54 +0300

    Create Category UI

---
+++ E-CommerceWeb/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using E_CommerceWeb.Data;
+using E_CommerceWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_CommerceWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
